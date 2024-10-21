daa69e6d0e4106e9ade4e903d1569162d2d8716a
Diego Estevão Lopes de Queiroz <deadzone8727@gmail.com>
2024-10-21 13:36:27 +0000

    Add initial car models to CarrosManager

--- src/main/java/br/dev/diego/weservice/CarrosManager.java
+++ src/main/java/br/dev/diego/weservice/CarrosManager.java
@@ -5,6 +5,9 @@ public class CarrosManager {
     private List<Carro> carros;
     public CarrosManager(){
         this.carros = new ArrayList<>();
+        this.carros.add(new Carro("Fusca", "Volkswagen"));
+        this.carros.add(new Carro("Onix", "Chevrolet"));
+
     }
     public List<Carro> getCarros(){
         return carros;
